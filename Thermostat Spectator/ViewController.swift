bd12e24e45e23ad0d6ce4bb9045ee256d87a1ece
christopherdumas <christopherdumas@me.com>
2015-11-01 13:01:58 -0800

    Initial Commit

---
+++ Thermostat Spectator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thermostat Spectator
+//
+//  Created by Christopher Dumas on 11/1/15.
+//  Copyright © 2015 Christopher Dumas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
